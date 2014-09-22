ef453e517a35ffaa8fddba5d3937fdc065903cfe
Tony Aldridge <tony@dabapps.com>
2014-09-22 14:02:17 +0100

    Started importing UI requirements

--- src/client.rs
+++ src/client.rs
@@ -1,10 +1,28 @@
+#![feature(phase)]
+
+#[phase(plugin, link)]
+extern crate conrod;
+extern crate piston;
 extern crate collections;
 extern crate string_telephone;
+extern crate opengl_graphics;
+extern crate sdl2_game_window;
+
+use opengl_graphics::{
+    Gl,
+    Texture,
+};
+
+use conrod::{
+    UIContext,
+};
 
 use collections::str::{Slice, Owned};
 use std::io::net::ip::{Ipv4Addr, SocketAddr};
-//use piston::{EventIterator, EventSettings, WindowSettings, NoWindow};
+use piston::{EventIterator, EventSettings, WindowSettings, graphics, Render};
+use piston::graphics::{AddColor, Draw};
 use string_telephone::{Client, ConnectionConfig, UserPacket, Command, PacketDisconnect, PacketConnect};
+use sdl2_game_window::WindowSDL2;
 
 fn deserializer(message: &Vec<u8>) -> String {
     match String::from_utf8_lossy(message.as_slice()) {
@@ -31,4 +49,35 @@ fn main() {
         },
         Err(e) => fail!("Failed to connect - {}", e)
     };
+
+    let opengl = piston::shader_version::opengl::OpenGL_3_2;
+    let mut window = WindowSDL2::new(
+        opengl,
+        WindowSettings {
+            title: "SuperMegaTag".to_string(),
+            size: [800, 600],
+            fullscreen: false,
+            exit_on_esc: true,
+            samples: 0,
+        }
+    );
+
+    let event_settings = EventSettings {
+        updates_per_second: 120,
+        max_frames_per_second: 60,
+    };
+    let ref mut gl = Gl::new(opengl);
+
+    let mut uic = UIContext::new("Dense-Regular.otf");
+    for e in EventIterator::new(&mut window, &event_settings) {
+        match e {
+            Render(args) => {
+                gl.viewport(0, 0, args.width as i32, args.height as i32);
+
+                let c = graphics::Context::abs(args.width as f64, args.height as f64);
+                c.rgb(1.0, 1.0, 1.0).draw(gl);
+            },
+            _ => {},
+        }
+    }
 }
