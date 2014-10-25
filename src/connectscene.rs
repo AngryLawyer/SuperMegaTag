81c15c4bc99a563e9887aad262085142f943b83d
Tony Aldridge <zaragopha@hotmail.com>
2014-10-25 18:00:41 +0100

    Improved connection failure reporting

--- src/connectscene.rs
+++ src/connectscene.rs
@@ -1,11 +1,12 @@
 use std::io::net::ip::{SocketAddr, Ipv4Addr};
+use std::io::IoError;
 use std::time::duration::Duration;
 use string_telephone::{Client, ConnectionConfig, ClientConnectionConfig};
 use scene::{Scene,BoxedScene};
 use gamescene::GameScene;
 use gamestate::GameState;
 use conrod::{
-    label,
+    Label,
     Color,
     WidgetMatrix,
     Button,
@@ -25,12 +26,13 @@ use packet;
 
 enum ConnectState {
     Disconnected,
-    Connecting(Receiver<Option<Client<packet::Packet>>>)
+    Connecting(Receiver<Result<Client<packet::Packet>, IoError>>)
 }
 
 pub struct ConnectScene {
     pub edit_ip: Vec<String>,
     try_connect: ConnectState,
+    error_message: Option<String>
 }
 
 impl ConnectScene {
@@ -38,7 +40,8 @@ impl ConnectScene {
     pub fn new() -> BoxedScene {
        box ConnectScene {
            edit_ip: vec!["127".to_string(), "0".to_string(), "0".to_string(), "1".to_string()],
-           try_connect: Disconnected
+           try_connect: Disconnected,
+           error_message: None
        }
     }
     
@@ -56,14 +59,15 @@ impl Scene for ConnectScene {
                 let should_disconnect = match self.try_connect {
                     Connecting(ref socket) => {
                         match socket.try_recv() {
-                            Ok(Some(comms)) => {
+                            Ok(Ok(comms)) => {
                                 println!("Connected");
                                 maybe_scene = Some(GameScene::new());
                                 state.set_comms(comms);
                                 true
                             },
-                            Ok(None) => {
+                            Ok(Err(e)) => {
                                 println!("Couldn't connect");
+                                self.error_message = Some(e.desc.to_string());
                                 true 
                             },
                             _ => false
@@ -82,20 +86,19 @@ impl Scene for ConnectScene {
                 gl.viewport(0, 0, args.width as i32, args.height as i32);
 
                 let c = &graphics::Context::abs(args.width as f64, args.height as f64);
-
-
                 c.rgb(1.0, 1.0, 1.0).draw(gl);
-                label::draw(
-                    gl,
-                    uic,
-                    [0f64, 0f64], // Screen position.
-                    48u32, // Font size.
-                    Color::new(1.0, 0.0, 0.0, 1.0),
-                    "Select a server"
-                );
+
+                let initial_pos = [((600u32 / 2) - 150) as f64, 0.0];
+
+                //Title label
+                uic.label("Select a server")
+                    .position(initial_pos[0], initial_pos[1])
+                    .size(48u32)
+                    .color(Color::black())
+                    .draw(gl);
 
                 uic.widget_matrix(4, 1)
-                    .position(0.0, 56.0)
+                    .position(initial_pos[0], initial_pos[1] + 56.0)
                     .dimensions(300.0, 240.0)
                     .each_widget(|uic, num, _, _, pos, dims| {
                         // Now draw the widgets with the given callback.
@@ -109,44 +112,60 @@ impl Scene for ConnectScene {
 
                     });
 
-                uic.button(7u64)
-                    .dimensions(90.0, 36.0)
-                    .position(0.0, 128.0)
-                    .color(Color::black())
-                    .frame(2.0)
-                    .label("Connect")
-                    .callback(|| {
-                        if self.is_connecting() == false {
-                            let parsed = (from_str(self.edit_ip[0].as_slice()), from_str(self.edit_ip[1].as_slice()), from_str(self.edit_ip[2].as_slice()), from_str(self.edit_ip[3].as_slice()));
+                if self.is_connecting() {
+                    uic.label("Connecting...")
+                        .position(initial_pos[0], initial_pos[1] + 128.0)
+                        .size(48u32)
+                        .color(Color::black())
+                        .draw(gl);
+                } else {
+                    uic.button(7u64)
+                        .dimensions(90.0, 36.0)
+                        .position(initial_pos[0], initial_pos[1] + 128.0)
+                        .color(Color::black())
+                        .label_color(Color::white())
+                        .frame(2.0)
+                        .label("Connect")
+                        .callback(|| {
+                            if self.is_connecting() == false {
+                                let parsed = (from_str(self.edit_ip[0].as_slice()), from_str(self.edit_ip[1].as_slice()), from_str(self.edit_ip[2].as_slice()), from_str(self.edit_ip[3].as_slice()));
 
-                            match parsed {
-                                (Some(a), Some(b), Some(c), Some(d)) => {
-                                    let (tx, rx) = channel();
-                                    self.try_connect = Connecting(rx);
-                                    spawn(proc() {
-                                        let settings = ConnectionConfig {
-                                            protocol_id: 88869,
-                                            timeout_period: Duration::seconds(10),
-                                            packet_deserializer: packet::deserializer,
-                                            packet_serializer: packet::serializer
-                                        };
+                                match parsed {
+                                    (Some(a), Some(b), Some(c), Some(d)) => {
+                                        self.error_message = None;
+                                        let (tx, rx) = channel();
+                                        self.try_connect = Connecting(rx);
+                                        spawn(proc() {
+                                            let settings = ConnectionConfig {
+                                                protocol_id: 88869,
+                                                timeout_period: Duration::seconds(10),
+                                                packet_deserializer: packet::deserializer,
+                                                packet_serializer: packet::serializer
+                                            };
 
-                                        let client_settings = ClientConnectionConfig::new(3, Duration::seconds(5));
+                                            let client_settings = ClientConnectionConfig::new(3, Duration::seconds(5));
 
-                                        println!("Connecting");
-                                        match Client::connect(SocketAddr {ip: Ipv4Addr(0, 0, 0, 0), port: 0}, SocketAddr {ip: Ipv4Addr(a, b, c, d), port: 8869}, settings, client_settings) {
-                                            Ok(client) => {
-                                                tx.send(Some(client))
-                                            },
-                                            Err(_) => tx.send(None)
-                                        };
-                                    });
-                                },
-                                _ => ()
-                            };
+                                            println!("Connecting");
+                                            tx.send(Client::connect(SocketAddr {ip: Ipv4Addr(0, 0, 0, 0), port: 0}, SocketAddr {ip: Ipv4Addr(a, b, c, d), port: 8869}, settings, client_settings));
+                                        });
+                                    },
+                                    _ => ()
+                                };
+                            }
+                        })
+                        .draw(gl);
+                }
+
+                match self.error_message {
+                    Some(ref error) => {
+                        uic.label(error.as_slice())
+                            .position(initial_pos[0], initial_pos[1] + 128.0 + 36.0)
+                            .size(48u32)
+                            .color(Color::black())
+                            .draw(gl);
                         }
-                    })
-                    .draw(gl);
+                    None => ()
+                }
                 None
             },
             _ => None,
