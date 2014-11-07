0547a79698a7c30ded84f525e092b86769cfebb2
Tony Aldridge <tony@dabapps.com>
2014-11-07 14:45:02 +0000

    Corrected paths

--- src/client.rs
+++ src/client.rs
@@ -40,9 +40,9 @@ fn main() {
     );
     let window = RefCell::new(window);
 
-    let context = UiContext::new(&Path::new("../assets/Dense-Regular.otf"), None).ok().expect("Couldn't get a graphics context!");
+    let context = UiContext::new(&Path::new("./assets/Dense-Regular.otf"), None).ok().expect("Couldn't get a graphics context!");
 
-    let mut gamestate = gamestate::GameState::new(context, Gl::new(opengl), &Path::new("../assets"));
+    let mut gamestate = gamestate::GameState::new(context, Gl::new(opengl), &Path::new("./assets"));
     let mut current_scene = connectscene::ConnectScene::new();
 
     for ref e in Events::new(&window) {
